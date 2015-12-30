63f52f01c7bca3474548d949c0b03c0899615bd1
tian <976969426@qq.com>
2015-12-30 16:39:43 +0800

    testp

--- zznetHadoop/src/main/java/tools/Constants.java
+++ zznetHadoop/src/main/java/tools/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public final static int MESSAGE_LENGTH = 18;
     public final static int KEY_NUM = 5;
     public final static int SENSITIVE_WORD_NUM = -1;
-    public final static int LAYER = 1;
+    public final static int LAYER = 2;
     public final static String NOISE = "noise";
     public final static double SIMILAR_SCORE_LINE = -0.5;
     public final static int TIME_OUT = 2000;
