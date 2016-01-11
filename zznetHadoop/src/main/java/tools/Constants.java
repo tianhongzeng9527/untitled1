4574f5e6e56b615b4abfce37ef3990871d3ae945
tian <tianhongzeng@qq.com>
2016-01-11 15:08:35 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	zznetHadoop/src/main/java/urlhandle/TitleKeyWords.java
    	zznetHadoop/src/main/java/usrhandle/UserDataInformation.java

--- zznetHadoop/src/main/java/tools/Constants.java
+++ zznetHadoop/src/main/java/tools/Constants.java
@@ -13,8 +13,8 @@ public class Constants {
     public final static double SIMILAR_SCORE_LINE = -0.5;
     public final static int TIME_OUT = 30000;
     public final static String SENSITIVE_CATEGORY = "sensitive";
-    public final static String SENSITIVE_URL = "http://192.168.1.106:8080/zh-school/api/sensitive.do";
-    public final static String CATEGORY_URL = "http://192.168.1.106:8080/zh-school/api/behavior.do";
+    public final static String SENSITIVE_URL = "http://192.168.1.106:8080/public_behavior/api/sensitive.do";
+    public final static String CATEGORY_URL = "http://192.168.1.106:8080/public_behavior/api/behavior.do";
     public final static String WORDS_DISTANCE_URL = "http://192.168.1.106:8080/tianhongzeng/wordsDistance?word1=%s&word2=%s";
     public final static String SIMILARWORDS_URL = "http://192.168.1.106:8080/tianhongzeng/wordsDistance?word=%s&num=%s";
     public final static int RETURN_TYPE_SENSITIVE = 1;
